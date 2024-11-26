ea76aeaaf0a2d2d1ec34d4afd876b74e7b8e152d
StyledStrike <lameulucas@gmail.com>
2024-11-26 12:08:24 -0300

    Add custom tabbed DFrame to the theme script

--- lua/styledstrike/theme.lua
+++ lua/styledstrike/theme.lua
@@ -422,3 +422,173 @@ function StyledTheme:CreateBinderButton( parent, text, defaultKey )
 
     return binder
 end
+
+----- Custom DFrame with tabs -----
+
+local TabButton = {}
+
+function TabButton:Init()
+    self:SetCursor( "hand" )
+    self.icon = vgui.Create( "DImage", self )
+
+    self.isSelected = false
+    self.notificationCount = 0
+    self.SColors = DEFAULT_COLORS
+end
+
+function TabButton:SetIcon( path )
+    self.icon:SetImage( path )
+end
+
+function TabButton:PerformLayout( w, h )
+    local size = math.max( w, h ) * 0.4
+
+    self.icon:SetSize( size, size )
+    self.icon:Center()
+end
+
+local COLOR_INDICATOR = Color( 200, 0, 0, 255 )
+
+function TabButton:Paint( w, h )
+    local colors = self.SColors
+
+    if self.isSelected then
+        SetDrawColor( colors.buttonPress:Unpack() )
+        DrawRect( 0, 0, w, h )
+    end
+
+    if self:IsHovered() then
+        SetDrawColor( colors.buttonHover:Unpack() )
+        DrawRect( 0, 0, w, h )
+    end
+
+    if self.notificationCount > 0 then
+        local size = 14
+        local x = w - size - 2
+        local y = h - size - 2
+
+        DrawRoundedBox( size * 0.5, x, y, size, size, COLOR_INDICATOR )
+        draw.SimpleText( self.notificationCount, "TargetIDSmall", x + size * 0.5, y + size * 0.5, colors.buttonText, 1, 1 )
+    end
+end
+
+function TabButton:OnMousePressed( keyCode )
+    if keyCode == MOUSE_LEFT then
+        self:GetParent():GetParent():SetActiveTab( self.tab )
+    end
+end
+
+vgui.Register( "Styled_TabButton", TabButton, "DPanel" )
+
+local PANEL = {}
+
+function PANEL:Init()
+    local w = math.max( ScrH() * 0.7, 600 )
+    local h = math.max( ScrH() * 0.45, 400 )
+
+    self:SetPos( 0, 0 )
+    self:SetSize( w, h )
+    self:SetSizable( true )
+    self:SetDraggable( true )
+    self:SetDeleteOnClose( true )
+    self:SetScreenLock( true )
+    self:SetMinWidth( 600 )
+    self:SetMinHeight( 400 )
+    self:DockPadding( 4, 28, 4, 4 )
+
+    self.btnClose:SetText( "X" )
+    self.btnMaxim:Remove()
+    self.btnMinim:Remove()
+
+    self.tabList = vgui.Create( "DPanel", self )
+    self.tabList:SetWide( 48 )
+    self.tabList:Dock( LEFT )
+    self.tabList:DockPadding( 2, 2, 2, 2 )
+    self.tabList:SetBackgroundColor( self.SColors.panelBackground )
+
+    self.tabContainer = vgui.Create( "DPanel", self )
+    self.tabContainer:Dock( FILL )
+    self.tabContainer:DockMargin( 4, 0, 0, 0 )
+    self.tabContainer:DockPadding( 0, 0, 0, 0 )
+    self.tabContainer:SetPaintBackground( false )
+
+    self.tabs = {}
+end
+
+function PANEL:ApplyTheme( theme )
+    theme:Apply( self.btnClose )
+    theme:Apply( self, "DFrame" )
+
+    for _, t in ipairs( self.tabs ) do
+        t.button.SColors = theme.colors
+    end
+end
+
+function PANEL:AddTab( icon, tooltip )
+    local tab = {}
+
+    tab.button = vgui.Create( "Styled_TabButton", self.tabList )
+    tab.button:SetIcon( icon )
+    tab.button:SetTall( 44 )
+    tab.button:SetTooltip( tooltip )
+    tab.button:Dock( TOP )
+    tab.button:DockMargin( 0, 0, 0, 2 )
+    tab.button.tab = tab
+    tab.button.SColors = self.SColors or DEFAULT_COLORS
+
+    tab.panel = vgui.Create( "DScrollPanel", self.tabContainer )
+    tab.panel:Dock( FILL )
+    tab.panel:DockMargin( 0, 0, 0, 0 )
+    tab.panel:DockPadding( 0, 0, 0, 0 )
+    tab.panel:SetPaintBackground( false )
+    tab.panel:SetVisible( false )
+    tab.panel.pnlCanvas:DockPadding( 0, -4, 4, 0 )
+
+    self.tabs[#self.tabs + 1] = tab
+
+    if #self.tabs == 1 then
+        self:SetActiveTab( tab )
+    end
+
+    return tab.panel
+end
+
+function PANEL:SetActiveTab( tab )
+    for i, t in ipairs( self.tabs ) do
+        local isThisOne = t == tab
+
+        t.button.isSelected = isThisOne
+        t.panel:SetVisible( isThisOne )
+
+        if isThisOne then
+            self.lastTabIndex = i
+        end
+    end
+end
+
+function PANEL:SetActiveTabByIndex( index )
+    if self.tabs[index] then
+        self:SetActiveTab( self.tabs[index] )
+    end
+end
+
+function PANEL:SetTabNotificationCountByIndex( index, count )
+    if self.tabs[index] then
+        self.tabs[index].button.notificationCount = count
+    end
+end
+
+function PANEL:PerformLayout()
+    local titlePush = 16
+
+    self.imgIcon:SetPos( 4, 5 )
+    self.imgIcon:SetSize( 16, 16 )
+
+    self.btnClose:SetPos( self:GetWide() - 28 - 2, 2 )
+    self.btnClose:SetSize( 28, 20 )
+
+    self.lblTitle:SetPos( 8 + titlePush, 2 )
+    self.lblTitle:SetSize( self:GetWide() - 25 - titlePush, 20 )
+end
+
+vgui.Register( "Styled_TabbedFrame", PANEL, "DFrame" )
