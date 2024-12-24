944a02b8c886c0fe9447262daba31c9980019b73
StyledStrike <lameulucas@gmail.com>
2024-12-24 17:01:58 -0300

    Tweak redline sound volume

--- lua/glide/client/engine_stream.lua
+++ lua/glide/client/engine_stream.lua
@@ -290,7 +290,7 @@ function EngineStream:Think( dt, eyePos, eyeRight )
     local redlineVol = 1
 
     if self.isRedlining then
-        redlineVol = 0.7 + Cos( Time() * self.redlineFrequency ) * 0.4
+        redlineVol = 0.75 + Cos( Time() * self.redlineFrequency ) * 0.25
     end
 
     local inputs = self.inputs
