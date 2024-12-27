099291a8d736150135e88469f84eef77dc3522e4
StyledStrike <lameulucas@gmail.com>
2024-12-26 22:18:02 -0300

    Save Wheel Model tool data on dupes

--- lua/weapons/gmod_tool/stools/glide_wheel_model.lua
+++ lua/weapons/gmod_tool/stools/glide_wheel_model.lua
@@ -19,6 +19,10 @@ TOOL.ClientConVar = {
 }
 
 local function ValidateModel( model )
+    if type( model ) ~= "string" then
+        return false
+    end
+
     if not file.Exists( model, "GAME" ) then
         return false
     end
@@ -32,33 +36,104 @@ local function IsGlideVehicle( ent )
     return IsValid( ent ) and ent.IsGlideVehicle
 end
 
-function TOOL:GetGlideWheels( trace )
+local GetWheels, ApplyWheelMods
+
+if SERVER then
+    GetWheels = function( ent )
+        if not IsGlideVehicle( ent ) then return false end
+
+        local frontWheels, rearWheels = {}, {}
+        local frontCount, rearCount = 0, 0
+        local hiddenCount = 0
+
+        if ent.wheels then
+            for _, w in ipairs( ent.wheels ) do
+                if IsValid( w ) then
+                    if w.GlideIsHidden then
+                        hiddenCount = hiddenCount + 1
+                    end
+
+                    if w.isFrontWheel then
+                        frontCount = frontCount + 1
+                        frontWheels[frontCount] = w
+                    else
+                        rearCount = rearCount + 1
+                        rearWheels[rearCount] = w
+                    end
+                end
+            end
+        end
+
+        return frontWheels, rearWheels, hiddenCount
+    end
+
+    local function ApplyToWheels( wheels, model, scale )
+        local data = list.Get( "GlideWheelModels" )[model]
+        if not data then return end
+
+        scale = ( data.scale or Vector( 1, 1, 0.35 ) ) * scale
+
+        local angle = data.angle or Angle( 0, 0, 270 )
+        local offset = data.offset or Vector( 0, 0, 0 )
+
+        for _, w in ipairs( wheels ) do
+            if not w.GlideIsHidden then
+                w.modelOverride = model
+                w.modelScale = scale
+                w:SetModel( model )
+                w:ChangeRadius()
+
+                if w.isOnRight then
+                    w:SetModelOffset( -offset )
+                    w:SetModelAngle( angle + Angle( 0, 180, 0 ) )
+                else
+                    w:SetModelOffset( offset )
+                    w:SetModelAngle( angle )
+                end
+            end
+        end
+    end
+
+    ApplyWheelMods = function( _ply, ent, data )
+        if not IsGlideVehicle( ent ) then return false end
+
+        local frontModel = data.FrontModel
+        local rearModel = data.RearModel
+
+        local frontScaleX = Glide.ValidateNumber( data.FrontScaleX, 0.1, 5, 1 )
+        local frontScaleY = Glide.ValidateNumber( data.FrontScaleY, 0.1, 5, 1 )
+        local frontScaleZ = Glide.ValidateNumber( data.FrontScaleZ, 0.1, 5, 1 )
+
+        local rearScaleX = Glide.ValidateNumber( data.RearScaleX, 0.1, 5, 1 )
+        local rearScaleY = Glide.ValidateNumber( data.RearScaleY, 0.1, 5, 1 )
+        local rearScaleZ = Glide.ValidateNumber( data.RearScaleZ, 0.1, 5, 1 )
+
+        if ValidateModel( frontModel ) and ValidateModel( rearModel ) then
+            local frontWheels, rearWheels = GetWheels( ent )
+
+            ApplyToWheels( frontWheels, frontModel, Vector( frontScaleX, frontScaleY, frontScaleZ ) )
+            ApplyToWheels( rearWheels, rearModel, Vector( rearScaleX, rearScaleY, rearScaleZ ) )
+
+            duplicator.StoreEntityModifier( ent, "glide_wheel_mods", data )
+
+            return true
+        end
+
+        return false
+    end
+
+    duplicator.RegisterEntityModifier( "glide_wheel_mods", ApplyWheelMods )
+end
+
+function TOOL:GetTraceGlideEntities( trace )
     local ent = trace.Entity
     if not IsGlideVehicle( ent ) then return false end
     if not SERVER then return ent end
 
     local owner = self:GetOwner()
-    local frontWheels, rearWheels = {}, {}
-    local frontCount, rearCount = 0, 0
-    local hiddenCount = 0
-
-    if ent.wheels then
-        for _, w in ipairs( ent.wheels ) do
-            if IsValid( w ) then
-                if w.GlideIsHidden then
-                    hiddenCount = hiddenCount + 1
-                end
-
-                if w.isFrontWheel then
-                    frontCount = frontCount + 1
-                    frontWheels[frontCount] = w
-                else
-                    rearCount = rearCount + 1
-                    rearWheels[rearCount] = w
-                end
-            end
-        end
-    end
+    local frontWheels, rearWheels, hiddenCount = GetWheels( ent )
+    local frontCount = #frontWheels
+    local rearCount = #rearWheels
 
     if hiddenCount == frontCount + rearCount then
         Glide.SendNotification( owner, {
@@ -85,71 +160,44 @@ function TOOL:GetGlideWheels( trace )
     return ent, frontWheels, rearWheels
 end
 
-function TOOL:ApplyModifications( wheels, model, scale )
-    if not ValidateModel( model ) then return end
-
-    local data = list.Get( "GlideWheelModels" )[model]
-    if not data then return end
-
-    scale = ( data.scale or Vector( 1, 1, 0.35 ) ) * scale
-
-    local angle = data.angle or Angle( 0, 0, 270 )
-    local offset = data.offset or Vector( 0, 0, 0 )
-
-    for _, w in ipairs( wheels ) do
-        w.modelOverride = model
-        w.modelScale = scale
-        w:SetModel( model )
-        w:ChangeRadius()
-
-        if w.isOnRight then
-            w:SetModelOffset( -offset )
-            w:SetModelAngle( angle + Angle( 0, 180, 0 ) )
-        else
-            w:SetModelOffset( offset )
-            w:SetModelAngle( angle )
-        end
-    end
-end
-
 function TOOL:LeftClick( trace )
-    local veh, frontWheels, rearWheels = self:GetGlideWheels( trace )
-    if not veh then return false end
+    local vehicle = self:GetTraceGlideEntities( trace )
+    if not vehicle then return false end
 
     if SERVER then
-        local frontModel = self:GetClientInfo( "front" )
-        local rearModel = self:GetClientInfo( "rear" )
+        ApplyWheelMods( self:GetOwner(), vehicle, {
+            FrontModel = self:GetClientInfo( "front" ),
+            RearModel = self:GetClientInfo( "rear" ),
 
-        local frontScale = Vector(
-            math.Clamp( self:GetClientNumber( "front_scale_x", 1 ), 0.1, 5 ),
-            math.Clamp( self:GetClientNumber( "front_scale_y", 1 ), 0.1, 5 ),
-            math.Clamp( self:GetClientNumber( "front_scale_z", 1 ), 0.1, 5 )
-        )
+            FrontScaleX = self:GetClientNumber( "front_scale_x", 1 ),
+            FrontScaleY = self:GetClientNumber( "front_scale_y", 1 ),
+            FrontScaleZ = self:GetClientNumber( "front_scale_z", 1 ),
 
-        local rearScale = Vector(
-            math.Clamp( self:GetClientNumber( "rear_scale_x", 1 ), 0.1, 5 ),
-            math.Clamp( self:GetClientNumber( "rear_scale_y", 1 ), 0.1, 5 ),
-            math.Clamp( self:GetClientNumber( "rear_scale_z", 1 ), 0.1, 5 )
-        )
-
-        self:ApplyModifications( frontWheels, frontModel, frontScale )
-        self:ApplyModifications( rearWheels, rearModel, rearScale )
+            RearScaleX = self:GetClientNumber( "rear_scale_x", 1 ),
+            RearScaleY = self:GetClientNumber( "rear_scale_y", 1 ),
+            RearScaleZ = self:GetClientNumber( "rear_scale_z", 1 )
+        } )
     end
 
     return true
 end
 
 function TOOL:RightClick( trace )
-    local veh, frontWheels, rearWheels = self:GetGlideWheels( trace )
-    if not veh then return false end
+    local vehicle, frontWheels, rearWheels = self:GetTraceGlideEntities( trace )
+    if not vehicle then return false end
 
     if SERVER then
-        local frontModel = frontWheels[1]:GetModel()
-        local rearModel = rearWheels[1]:GetModel()
         local ply = self:GetOwner()
 
-        ply:ConCommand( "glide_wheel_model_front " .. frontModel )
-        ply:ConCommand( "glide_wheel_model_rear " .. rearModel )
+        if #frontWheels > 0 then
+            local frontModel = frontWheels[1]:GetModel()
+            ply:ConCommand( "glide_wheel_model_front " .. frontModel )
+        end
+
+        if #rearWheels > 0 then
+            local rearModel = rearWheels[1]:GetModel()
+            ply:ConCommand( "glide_wheel_model_rear " .. rearModel )
+        end
     end
 
     return true
