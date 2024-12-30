dacae22464304ecb63b7d4469aca0587ce2cd010
Redox <git@redox.to>
2024-12-30 06:04:01 +0100

    Comment

--- lua/glide/server/weaponry.lua
+++ lua/glide/server/weaponry.lua
@@ -265,7 +265,7 @@ local function isLockableEntity( ent, skipParentCheck )
     local class = getClass( ent )
     if class == "prop_vehicle_prisoner_pod" and not skipParentCheck then -- Checks for parent vehicles like for example glide
         local parent = getParent( ent )
-        if parent ~= NULL then
+        if parent ~= NULL then -- Check directly against NULL as getParent returns a clean NULL object and it's faster than IsValid
             if isLockableEntity( parent, true ) then
                 return false
             end
