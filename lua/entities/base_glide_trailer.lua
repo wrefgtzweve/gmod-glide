460c647e72b62b2eeead60a829f1054c38ef2cda
StyledStrike <lameulucas@gmail.com>
2025-04-01 18:23:18 -0300

    Cleanup

--- lua/entities/base_glide_trailer.lua
+++ lua/entities/base_glide_trailer.lua
@@ -26,77 +26,77 @@ function ENT:IsReversing()
     return self:GetIsVehicleReversing()
 end
 
-if SERVER then
-    ENT.BlastDamageMultiplier = 0
-    ENT.BulletDamageMultiplier = 0
-    ENT.CollisionDamageMultiplier = 0
+if not SERVER then return end
 
-    -- Trailer sounds
-    ENT.AttachSound = "buttons/lever3.wav"
-    ENT.AttachSoundPitch = 85
-    ENT.DetachSound = "buttons/lever5.wav"
-    ENT.DetachSoundPitch = 90
+ENT.BlastDamageMultiplier = 0
+ENT.BulletDamageMultiplier = 0
+ENT.CollisionDamageMultiplier = 0
 
-    --- Implement this base class function.
-    function ENT:OnPostInitialize()
-        -- Trigger wire outputs
-        if WireLib then
-            WireLib.TriggerOutput( self, "IsAttached", 0 )
-        end
+-- Trailer sounds
+ENT.AttachSound = "buttons/lever3.wav"
+ENT.AttachSoundPitch = 85
+ENT.DetachSound = "buttons/lever5.wav"
+ENT.DetachSoundPitch = 90
+
+--- Implement this base class function.
+function ENT:OnPostInitialize()
+    -- Trigger wire outputs
+    if WireLib then
+        WireLib.TriggerOutput( self, "IsAttached", 0 )
+    end
+end
+
+--- Override this base class function.
+function ENT:SetupWiremodPorts( inputs, outputs )
+    BaseClass.SetupWiremodPorts( self, inputs, outputs )
+
+    outputs[#outputs + 1] = { "IsAttached", "NORMAL", "0: Detached\n1: Attached to a vehicle" }
+end
+
+function ENT:UpdateWiremodOutputs()
+    if WireLib then
+        WireLib.TriggerOutput( self, "IsAttached", self:GetIsAttached() and 1 or 0 )
+    end
+end
+
+--- Override this base class function.
+function ENT:OnSocketConnect( socket, otherVehicle )
+    if not socket.isReceptacle then
+        self.attachedVehicle = otherVehicle
+        self:EmitSound( self.AttachSound, 80, self.AttachSoundPitch, 1.0 )
     end
 
-    --- Override this base class function.
-    function ENT:SetupWiremodPorts( inputs, outputs )
-        BaseClass.SetupWiremodPorts( self, inputs, outputs )
+    self:SetIsAttached( true )
+    self:UpdateWiremodOutputs()
+end
 
-        outputs[#outputs + 1] = { "IsAttached", "NORMAL", "0: Detached\n1: Attached to a vehicle" }
+--- Override this base class function.
+function ENT:OnSocketDisconnect( socket )
+    if not socket.isReceptacle then
+        self.attachedVehicle = nil
+        self:EmitSound( self.DetachSound, 80, self.DetachSoundPitch, 0.9 )
     end
 
-    function ENT:UpdateWiremodOutputs()
-        if WireLib then
-            WireLib.TriggerOutput( self, "IsAttached", self:GetIsAttached() and 1 or 0 )
-        end
+    self:SetIsAttached( false )
+    self:UpdateWiremodOutputs()
+end
+
+--- Override this base class function.
+function ENT:OnPostThink( _dt, selfTbl )
+    local attachedVehicle = selfTbl.attachedVehicle
+
+    if not IsValid( attachedVehicle ) then
+        self:SetIsBraking( false )
+        self:SetHeadlightState( 0 )
+        self:SetTurnSignalState( 0 )
+        self:SetIsVehicleReversing( false )
+
+        return
     end
 
-    --- Override this base class function.
-    function ENT:OnSocketConnect( socket, otherVehicle )
-        if not socket.isReceptacle then
-            self.attachedVehicle = otherVehicle
-            self:EmitSound( self.AttachSound, 80, self.AttachSoundPitch, 1.0 )
-        end
-
-        self:SetIsAttached( true )
-        self:UpdateWiremodOutputs()
-    end
-
-    --- Override this base class function.
-    function ENT:OnSocketDisconnect( socket )
-        if not socket.isReceptacle then
-            self.attachedVehicle = nil
-            self:EmitSound( self.DetachSound, 80, self.DetachSoundPitch, 0.9 )
-        end
-
-        self:SetIsAttached( false )
-        self:UpdateWiremodOutputs()
-    end
-
-    --- Override this base class function.
-    function ENT:OnPostThink( _dt, selfTbl )
-        local attachedVehicle = selfTbl.attachedVehicle
-
-        if not IsValid( attachedVehicle ) then
-            self:SetIsBraking( false )
-            self:SetHeadlightState( 0 )
-            self:SetTurnSignalState( 0 )
-            self:SetIsVehicleReversing( false )
-
-            return
-        end
-
-        -- Copy lights from the attached vehicle
-        self:SetIsBraking( attachedVehicle:GetIsBraking() )
-        self:SetHeadlightState( attachedVehicle:GetHeadlightState() )
-        self:SetTurnSignalState( attachedVehicle:GetTurnSignalState() )
-        self:SetIsVehicleReversing( attachedVehicle:IsReversing() )
-    end
+    -- Copy lights from the attached vehicle
+    self:SetIsBraking( attachedVehicle:GetIsBraking() )
+    self:SetHeadlightState( attachedVehicle:GetHeadlightState() )
+    self:SetTurnSignalState( attachedVehicle:GetTurnSignalState() )
+    self:SetIsVehicleReversing( attachedVehicle:IsReversing() )
 end
