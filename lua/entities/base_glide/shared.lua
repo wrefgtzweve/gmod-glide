10561ee17d89062ba1e57b79de45a494e2f5f978
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2025-06-17 23:15:05 +0300

    Revert some changes

--- lua/entities/base_glide/shared.lua
+++ lua/entities/base_glide/shared.lua
@@ -113,10 +113,6 @@ function ENT:GravGunPickupAllowed( _ply )
     return false
 end
 
-function ENT:IsVehicle()
-    return true
-end
-
 -- You can safely override these on children classes
 function ENT:IsEngineOn()
     return self:GetEngineState() > 1
