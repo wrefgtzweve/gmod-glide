f7104e208390d4a2f628f9482dc3913e82128a5a
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2025-06-17 23:16:03 +0300

    Make it better

--- lua/autorun/sh_glide.lua
+++ lua/autorun/sh_glide.lua
@@ -394,6 +394,17 @@ do
     end
 end
 
+hook.Add( "Initialize", "Glide.OverrideIsVehicle", function()
+    local EntityMeta = FindMetaTable( "Entity" )
+    local IsVehicle = EntityMeta.IsVehicle
+
+    --- Override `Entity:IsVehicle` to return `true` on Glide vehicles.
+    --- Also keep compatibility with Simfphys.
+    function EntityMeta:IsVehicle()
+        return self.IsGlideVehicle or IsVehicle( self )
+    end
+end )
+
 local function IncludeDir( dirPath, doInclude, doTransfer )
     local files = file.Find( dirPath .. "*.lua", "LUA" )
     local path
